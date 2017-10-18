c033442ff88fb2706fc8dee858dd172eef955ca0
angrha <angrha@gmail.com>
2017-10-18 17:33:14 +0700

    first commit, done

---
+++ stringToArray.js
@@ -0,0 +1,15 @@
+function stringToArray(str){
+  var arrStr = str.split(',');
+  // console.log(arrStr.length)
+  var arr = []
+  for(var i = 0; i < 1; i++){
+    // console.log(arrStr[i])
+    for(var j = 0; j < arrStr.length; j++){
+      // console.log(arrStr[j])
+      arr.push(arrStr[j].split(''))
+    }
+  }
+  return arr
+}
+console.log(stringToArray('aqrst,ukaei,ffooo'))
+console.log(stringToArray('qwer,tyui,asdf,ghjk'))
